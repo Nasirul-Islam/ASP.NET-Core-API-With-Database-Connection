67d84c94c0a7b2d59c5331ee8a077a962be13a36
Nasirul Islam <nasirulislam866@gmail.com>
2024-10-17 11:26:34 +0600

    Configure CORS policy

--- API&DB/Program.cs
+++ API&DB/Program.cs
@@ -6,12 +6,43 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+// Configure CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin()   // Allows any origin
+               .AllowAnyMethod()   // Allows any HTTP method (GET, POST, etc.)
+               .AllowAnyHeader();  // Allows any headers
+    });
+});
+
+/*
+//Restrict CORS to Specific Origins (More Secure)
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins", builder =>
+    {
+        builder.WithOrigins("http://your-client-app.com") // Allow only your specific client URL
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+*/
+
 // Register UserService in Program.cs for Dependency Injection
 builder.Services.AddScoped<UsersService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure IIS options
+builder.Services.Configure<IISServerOptions>(options =>
+{
+    options.MaxRequestBodySize = 104857600; // 100 MB
+});
 
 var app = builder.Build();
 
@@ -21,6 +52,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Enable CORS for your API
+app.UseCors("AllowAllOrigins");
+
+//app.UseCors("AllowSpecificOrigins");
 
 app.UseHttpsRedirection();
 
